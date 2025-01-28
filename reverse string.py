430a08c4b0e45b4e65e0a55cb2fbd056194b9d45
Nur-A-Sabah <sajedurkabir@gmail.com>
2025-01-28 06:51:38 -0800

    first commit

---
+++ reverse string.py
@@ -0,0 +1,10 @@
+string = input("please Enter your own string: ")
+
+string2 = ""
+
+
+for i in string:
+    string2 = i + string2
+    
+print('\nThe original string = ', string)
+print('The reversed string = ', string2)
